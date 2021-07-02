e5adaeb03ba0b2953dd449c7f5c11a9bab5ac6c5
quentinepitechdigital <quentin.taverne@epitech.digital>
2021-07-02 14:17:02 +0200

    0.1.3
    Navbar/user/login
    Replaced fake inputs by real inputs
    Connexion button from btn-light to btn-dark

--- src/components/navbar/user/Login.vue
+++ src/components/navbar/user/Login.vue
@@ -10,17 +10,13 @@
 
         <div class="shadow p-3 mb-3 bg-secondary AVenirCard text-dark">
           <img class="d-flex justify-content-end mb-4" src="@/assets/icons/email_1.png" alt="Email">
-          <div class="shadow p-3 mb-3 bg-light text-secondary AVenirCard2">
-            <p>Adresse mail</p>
-          </div>
-          <div class="shadow p-3 mb-3 bg-light text-secondary AVenirCard2">
-            <p>Mot de passe</p>
-          </div>
-          <button type="button" class="btn btn-light AVenirCard">
-            Connexion
-          </button>
-          <p class="mt-3 fs-6 text-decoration-underline">Première connexion ?</p>
+          <input type="email" class="shadow p-3 mb-3 bg-light text-secondary AVenirCard2" placeholder="Adresse mail" />
+          <input type="text" class="shadow p-3 mb-3 bg-light text-secondary AVenirCard2"  placeholder="Mot de passe"/>
         </div>
+        <button type="button" class="btn btn-dark AVenirCard">
+          Connexion
+        </button>
+        <p class="mt-3 fs-6 text-decoration-underline">Première connexion ?</p>
       </div>
     </div>
 </template>
