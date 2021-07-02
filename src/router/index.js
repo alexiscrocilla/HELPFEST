8629762245f069b9e05f10270ebfd3e7f467059d
Alexis Crocilla <alexis.crocilla@epitech.digital>
2021-07-02 11:38:31 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
+
 const routes = [
   {
     path: '/',
@@ -8,12 +9,19 @@ const routes = [
     component: Home
   },
   {
+    path: '/staff',
+    name: 'Staff',
+    component: () => import('../views/Staff.vue')
+  },
+  {
+    path: '/user',
+    name: 'user',
+    component: () => import('../views/Userpanel.vue')
+  },
+  {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
   },
 ]
 
