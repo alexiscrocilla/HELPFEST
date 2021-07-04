51b7c84e7bd1cf44bb7d46c5b6138c1ab3b0d8eb
Alexis Crocilla <alexis.crocilla@epitech.digital>
2021-07-04 17:39:09 +0200

    Front Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,18 @@
 <template>
     <h1> Login to Your Account </h1>
-    <p> <input type='text' placeholder="Email" v-model='email'/> </p>
-    <p> <input type='password' placeholder="Password" v-model='password'/> </p>
+
+  <form class="p-3">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" v-model='email'>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model='password'>
+    </div>
     <p v-if="errMsg"> {{ errMsg }} </p>
-    <p> <button @click="signIn"> Submit </button> </p>
+    <button type="submit" class="btn btn-info" @click="signIn">Log-In</button>
+  </form>
 </template>
 
 <script setup>
@@ -14,7 +23,7 @@ const email = ref('')
 const password = ref('')
 const errMsg = ref() // ERROR MESSAGE
 const router = useRouter() // get a reference to our vue router
-const signIn = () => { // we also renamed this method 
+const signIn = () => { // we also renamed this method
   firebase
     .auth()
     .signInWithEmailAndPassword(email.value, password.value) // THIS LINE CHANGED
@@ -32,7 +41,7 @@ const signIn = () => { // we also renamed this method
             break
         case 'auth/wrong-password':
             errMsg.value = 'Incorrect password'
-            break  
+            break
         default:
             errMsg.value = 'Email or password was incorrect'
             break
