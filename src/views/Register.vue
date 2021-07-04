73ce39df48e5400a21051e35eafd8ced7d7b4e5e
Alexis Crocilla <alexis.crocilla@epitech.digital>
2021-07-04 17:39:15 +0200

    front register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,8 +1,17 @@
 <template>
     <h1> Create an Account </h1>
-    <p> <input type='text' placeholder="Email" v-model='email'/> </p>
-    <p> <input type='password' placeholder="Password" v-model='password'/> </p>
-    <p> <button @click="register"> Submit </button> </p>
+  <form class="p-3">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" v-model='email'>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model='password'>
+    </div>
+    <button type="submit" class="btn btn-success" @click="register">Register</button>
+  </form>
+
 </template>
 
 <script setup>
